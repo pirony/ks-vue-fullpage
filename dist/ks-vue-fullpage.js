c60f03563b8126334cb95d528dbf535db420f2c3
pirony <romainpouchol@gmail.com>
2017-10-05 19:18:07 +0200

    Fixed: menu should not anim on every transition

--- dist/ks-vue-fullpage.js
+++ dist/ks-vue-fullpage.js
@@ -1881,7 +1881,7 @@ if (false) {
 
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('div', _vm._l((_vm.navPosList), function(pos) {
-    return (_vm.currentPos === pos && (_vm.$ksvuefp.fpLoaded && !_vm.$ksvuefp.slidingActive)) ? _c('dots-anim', {
+    return (_vm.currentPos === pos && _vm.$ksvuefp.fpLoaded) ? _c('dots-anim', {
       key: pos,
       class: ['ksvuefp-nav', 'is-' + _vm.currentPos],
       attrs: {

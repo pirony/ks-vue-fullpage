c3e03f885188462d0969e1b95917d2698ba2dd40
pirony <romainpouchol@gmail.com>
2017-10-20 15:09:52 +0200

    Nuxt generate issue seems fixed

--- src/index.js
+++ src/index.js
@@ -4,86 +4,86 @@ import utils from './utils'
 import Vue from 'vue'
 import options from './defaultOptions'
 
-// function plugin (Vue) {
-  Vue.prototype.$ksvuefp = new Vue({
-    data: {
-      fpLoaded: false,
-      currentIndex: 0,
-      slidingActive: false,
-      sliderDirection: 'down',
-      wWidth: 0,
-      wHeight: 0,
-      options: {}
-    },
-    created () {
+function plugin (Vue) {
+Vue.prototype.$ksvuefp = new Vue({
+  data: {
+    fpLoaded: false,
+    currentIndex: 0,
+    slidingActive: false,
+    sliderDirection: 'down',
+    wWidth: 0,
+    wHeight: 0,
+    options: {}
+  },
+  created () {
+    const vm = this
+    vm.$on('ksvuefp-ready', () => {
+      vm.$emit('ksvuefp-resized')
+      vm.fpLoaded = true
+    })
+    vm.$on('ksvuefp-options-changed', (custom) => {
+      vm.options = { ...options, ...custom }
+    })
+
+    vm.$on('ksvuefp-resized', () => {
+      vm.getWindowDim()
+    })
+    vm.$on('ksvuefp-destroy', () => {
+      vm.fpLoaded = false
+      vm.currentIndex = 0
+      vm.slidingActive = false
+      vm.sliderDirection = 'down'
+      vm.options = {}
+    })
+
+    vm.$on('ksvuefp-change-begin', (nextIndex, oldIndex, direction, delay) => {
+      vm.slidingActive = true
+      vm.sliderDirection = direction
+      vm.$nextTick(() => {
+        setTimeout(() => {
+          vm.currentIndex = nextIndex
+        }, delay || 0)
+      })
+    })
+
+    vm.$on('ksvuefp-change-done', () => {
+      vm.slidingActive = false
+    })
+  },
+  methods: {
+    getWindowDim () {
       const vm = this
-      vm.$on('ksvuefp-ready', () => {
-        vm.$emit('ksvuefp-resized')
-        vm.fpLoaded = true
-      })
-      vm.$on('ksvuefp-options-changed', (custom) => {
-        vm.options = { ...options, ...custom }
-      })
-
-      vm.$on('ksvuefp-resized', () => {
-        vm.getWindowDim()
-      })
-      vm.$on('ksvuefp-destroy', () => {
-        vm.fpLoaded = false
-        vm.currentIndex = 0
-        vm.slidingActive = false
-        vm.sliderDirection = 'down'
-        vm.options = {}
-      })
-
-      vm.$on('ksvuefp-change-begin', (nextIndex, oldIndex, direction, delay) => {
-        vm.slidingActive = true
-        vm.sliderDirection = direction
-        vm.$nextTick(() => {
-          setTimeout(() => {
-            vm.currentIndex = nextIndex
-          }, delay || 0)
-        })
-      })
-
-      vm.$on('ksvuefp-change-done', () => {
-        vm.slidingActive = false
+      const Dimensions = utils.getWindowDim()
+      vm.wWidth = Dimensions.wWidth
+      vm.wHeight = Dimensions.wHeight
+      vm.$nextTick(() => {
+        vm.$ksvuefp.$emit('ksvuefp-change-done')
       })
     },
-    methods: {
-      getWindowDim () {
-        const vm = this
-        const Dimensions = utils.getWindowDim()
-        vm.wWidth = Dimensions.wWidth
-        vm.wHeight = Dimensions.wHeight
-        vm.$nextTick(() => {
-          vm.$ksvuefp.$emit('ksvuefp-change-done')
-        })
-      },
-      canAnimContent (index, wait = true) {
-        if (index !== this.currentIndex) return
-        if (wait) {
-          if (this.slidingActive) return
-          return true
-        }
+    canAnimContent (index, wait = true) {
+      if (index !== this.currentIndex) return
+      if (wait) {
+        if (this.slidingActive) return
         return true
       }
+      return true
     }
-  })
-  Vue.component('ksvuefp', ksvuefp)
-  Vue.component('ksvuefp-section', ksvuefpSection)
-// }
-//
-// // Install by default if using the script tag
-// if (typeof window !== 'undefined' && window.Vue) {
-//   window.Vue.use(plugin)
-// }
-//
-// export default plugin
-// const version = '__VERSION__'
-// // Export all components too
-// export {
-//   ksvuefp,
-//   ksvuefpSection,
-//   version
-// }
+  }
+})
+Vue.component('ksvuefp', ksvuefp)
+Vue.component('ksvuefp-section', ksvuefpSection)
+}
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+// Export all components too
+export {
+  ksvuefp,
+  ksvuefpSection,
+  version
+}
